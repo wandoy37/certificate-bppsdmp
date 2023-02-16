cb9afa3b9b2072c98c1c0925c50259d2bc52fc3f
wandoy37 <riswandi035@gmail.com>
2023-02-16 09:16:22 +0800

    layouting manage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('home.layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                this is writer your contents
+            </div>
+        </div>
+    </div>
+@endsection
